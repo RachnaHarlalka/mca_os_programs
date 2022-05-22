1710a5e5698ac531e0cf44467b9287d1094a969f
Rachna <rachnaag1999@gmail.com>
2022-05-22 12:29:47 +0530

    assignment 2 completed

--- ass2/2c_prior2.c
+++ ass2/2c_prior2.c
@@ -13,8 +13,9 @@ int priority(process*,int);
 int main()
 {
         int no_of_process,i,status;
-        float avgTAT=0.0, avgWT=0.0;
+        // float avgTAT=0.0, avgWT=0.0;
         process *processArray;
+        printf("..........PREEMTIVE PRIORITY SCHEDULING...........\n ");
         printf("Enter the no. of processes ");
         scanf("%d",&no_of_process);
         processArray=(process*)malloc(sizeof(process)*no_of_process);
@@ -50,21 +51,8 @@ int main()
         else 
                 printf("Succcessfull !!");
                 
-        printf("\n --------------------------------\n");
-        printf("\n\n");
-        printf("Process\t  Arrival Time\tBurst Time\tTurn Around Time\tWaiting Time\n");
-
-        for (int i = 0; i < no_of_process; i++)
-        {
-                avgWT += processArray[i].waiting_time;
-                avgTAT += processArray[i].turn_around_time;
-                printf("P%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processArray[i].process_id, processArray[i].arrival_time, processArray[i].burst_time, processArray[i].turn_around_time, processArray[i].waiting_time);
-        }
-
-        printf("\n");
-        printf("Average Waiting time : %.2f\n", avgWT / no_of_process);
-        printf("Average Turn around time : %.2f\n", avgTAT / no_of_process);
-
+       
+        free(processArray);
         return 0;
 
 }
@@ -96,7 +84,8 @@ int priority(process*p,int n)
 {
         int *remaining_BT;
         int i,isDone=0,index,morePriority,current_time=0,prev=-1;
-       
+               float avgTAT=0.0, avgWT=0.0;
+
         remaining_BT = (int *)malloc(sizeof(int) * n);
         if(remaining_BT == NULL)
         {
@@ -123,35 +112,47 @@ int priority(process*p,int n)
                                 {
                                         morePriority=p[i].priority;
                                         index=i;
-                                        printf("hello i am inside priority more\n");
+                                        // printf("hello i am inside priority more\n");
 
                                 }
                         }
                 } 
                 if(index!=-1)
                 {
-                        if (prev != index)
+                        if (index != prev)
                                 {
-                                printf("P%d ", p[index].process_id);
+                                printf("| P%d |", p[index].process_id);
                                 }
-                        remaining_BT[index]--;
-                        current_time++;
                         prev = index;
+                        current_time++;
+                        remaining_BT[index]--;
 
-                        if (p[index].burst_time == 0)
+                        if (remaining_BT[index] == 0)
                         {
                                 p[index].completion_time = current_time;
                                 p[index].turn_around_time = p[index].completion_time - p[index].arrival_time;
                                 p[index].waiting_time = p[index].turn_around_time - p[index].burst_time;
                                 p[index].isDone = 1;
                                 isDone++;
-                                printf("hello i am inside bt==0");
+                                // printf("hello i am inside bt==0");
                         }
                 }
                 else
                         current_time++;
         }
-        free(remaining_BT);
+        
+         printf("\n");
+        printf("Process\tPN\tAT\tBT\tWT\t TAT\n");
+        for(i=0;i<n;i++)
+        {
+                printf("%d\t%d\t%d\t%d\t%d\t%d\n",p[i].process_id,p[i].priority,p[i].arrival_time,p[i].burst_time,p[i].waiting_time,p[i].turn_around_time);
+
+                avgWT+=p[i].waiting_time;
+                avgTAT+=p[i].turn_around_time;
+        }
+        printf("Average Waiting Time:%.2f\n",avgWT/n);
+
+        printf("Average Turn Around Time:%.2f\n",avgTAT/n);
 
         return 0;
 }
